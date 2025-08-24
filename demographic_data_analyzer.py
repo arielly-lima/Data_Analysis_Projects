8c946c047f044b5525c8d0cd88435f27d2b1a771
Maria Arielly <maria.lima@sou.inteli.edu.br>
2025-08-24 18:43:08 +0000

    primeiro commit

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,44 +1,43 @@
 import pandas as pd
 
-
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
+    race_count = df['race'].value_counts()
 
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df.loc[df['sex'] == 'Male', 'age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = round((df['education'].value_counts(normalize=True) * 100)['Bachelors'], 1) #normalize=True é usado para calcular a porcentagem de cada valor
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
-    # What percentage of people without advanced education make more than 50K?
+    # filtra as pessoas que tem educação avancada
+    higher_education = df['education'].isin(['Bachelors', 'Masters', 'Doctorate'])
 
-    # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    lower_education = ~higher_education
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    # Filtra as pessoas que tem educação avancada e salário > 50K
+    higher_education_rich = round((df[higher_education]['salary'] == '>50K').mean() * 100, 1)
+    lower_education_rich = round((df[lower_education]['salary'] == '>50K').mean() * 100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
-
-    rich_percentage = None
+    num_min_workers = (df[df['hours-per-week'] == min_work_hours]['salary'] == '>50K').mean() * 100
 
     # What country has the highest percentage of people that earn >50K?
-    highest_earning_country = None
-    highest_earning_country_percentage = None
+    rich = df['salary'] == '>50K'
+    rich_per_country = rich.groupby(df['native-country']).mean() * 100
+    highest_earning_country = rich_per_country.idxmax()
+    highest_earning_country_percentage = round((rich_per_country).max(), 1)
 
     # Identify the most popular occupation for those who earn >50K in India.
-    top_IN_occupation = None
+    top_IN_occupation = df[(df['native-country'] == 'India') & (df['salary'] == '>50K')]['occupation'].value_counts().idxmax()
 
     # DO NOT MODIFY BELOW THIS LINE
 
@@ -49,7 +48,7 @@ def calculate_demographic_data(print_data=True):
         print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
         print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
         print(f"Min work time: {min_work_hours} hours/week")
-        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        print(f"Percentage of rich among those who work fewest hours: {num_min_workers}%")
         print("Country with highest percentage of rich:", highest_earning_country)
         print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
         print("Top occupations in India:", top_IN_occupation)
@@ -61,7 +60,7 @@ def calculate_demographic_data(print_data=True):
         'higher_education_rich': higher_education_rich,
         'lower_education_rich': lower_education_rich,
         'min_work_hours': min_work_hours,
-        'rich_percentage': rich_percentage,
+        'rich_percentage': num_min_workers,
         'highest_earning_country': highest_earning_country,
         'highest_earning_country_percentage':
         highest_earning_country_percentage,
